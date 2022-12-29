d8e07fe7f62540411a9ee22e00f4b0717fc02aeb
Brendan. Pham  - CMC Global DU3.1 <ptnghia1@cmcglobal.vn>
2022-12-29 17:02:48 +0700

    ReactJS 1commit

--- ReactJS-Udemy/src/App.js
+++ ReactJS-Udemy/src/App.js
@@ -1,13 +1,13 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <h2>Hello!</h2>
-        <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <h2>Hello!</h2>
+            <ExpenseItem></ExpenseItem>
+        </div>
+    );
 }
 
 export default App;
--- ReactJS-Udemy/src/components/ExpenseItem.js
+++ ReactJS-Udemy/src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
 function ExpenseItem() {
     return (
-        <h2>This is Expense component</h2>
+        <div>
+            <div>Mảch 28 2022</div>
+            <div>
+                <h2> car Insurance</h2>
+                <div> 1000000$</div>
+            </div>
+            <h2>This is Expense component</h2>
+
+        </div>
     );
 }
 export default ExpenseItem;
